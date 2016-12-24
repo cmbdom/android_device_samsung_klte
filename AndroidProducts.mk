2391085675e7362f31d0c009303516b9c155972e
Dominic Devio <djdevio@gmail.com>
2016-12-24 00:14:59 -0500

    more DU

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,6 +15,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_klte.mk \
     $(LOCAL_DIR)/du.mk
 
--- du.mk
+++ du.mk
@@ -1,6 +1,9 @@
-$(call inherit-product, vendor/du/configs/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/klte/full_klte.mk)
 
 PRODUCT_DEVICE := klte
 PRODUCT_NAME := du_klte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GM-900F
+PRODUCT_MANUFACTURER := Samsung
